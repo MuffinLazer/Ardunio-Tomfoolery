0aaa03fc4b9a133bcabbb053b0e5f894e3c23f79
MuffinLazer <muffinlaserr.gmail.com>
2025-02-11 13:29:23 -0500

    fixing 2Servo file due to programing issues, should be resloved

--- 2Servos/2Servos.ino
+++ 2Servos/2Servos.ino
@@ -1,20 +1,15 @@
 #include <Servo.h>
 #include <stdio.h>
 
-
 Servo Servo1;
 Servo Servo2;
-
 
 int servoPinA = 9;
 int servoPinB = 10;
 int potPin = A0;
 
-
-
-
 void setup() {
-    Serial.begin(96070);
+    Serial.begin(9600);
     Servo1.attach(servoPinA);
     Servo2.attach(servoPinB);
 }
@@ -24,11 +19,10 @@ void setup() {
 
 void loop() {
     int reading = analogRead(potPin);
-    int angle = map(reading, 0, 1023, 0, 180);
+    int angle = map(reading, 0, 1023, 0, 180 /10) *10;
     Servo1.write(angle);
+    Servo2.write(180 - angle);
     Serial.print(angle);
     Serial.print("\n");
-
-
-    Servo2.write(180 - angle);
+    delay(100);
 }
