47c8dfcb927837dbc5717bbf22a717d331917988
MuffinLazer <muffinlaserr@gmail.com>
2025-02-14 12:16:13 -0500

    Makeing the cargadbasforhealf files be able to control 2sets of servos individualy

--- cargadbasforhealf/cargadbasforhealf.ino
+++ cargadbasforhealf/cargadbasforhealf.ino
@@ -1,16 +1,18 @@
-int motorPin = 6;   
+int motorPinA = 6;
+int motorPinB = 9;
 int potPin = A0;   
 
 void setup() {
-  Serial.begin(9600);   
-  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(motorPinA, OUTPUT);
+  pinMode(motorPinB, OUTPUT);
 }
 
 void loop() {
   int reading = analogRead(potPin); 
-  int motorSpeed = map(reading, 0, 1024, 0, 1020); 
-
-  analogWrite(motorPin, motorSpeed);
+  int motorSpeed = map(reading, 0, 1023, 0, 1020); 
+  analogWrite(motorPinA, motorSpeed);
+  analogWrite(motorPinB, -motorSpeed);
 
   Serial.print("Motor Speed: ");
   Serial.println(motorSpeed);  
